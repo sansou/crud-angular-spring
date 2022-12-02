25865ff5d5ba6e2e2572569f5cb95fa66e335829
Thales Souza <santos.thales89@gmail.com>
2022-12-01 23:45:48 -0300

    [#14] - Hello Word com spring

---
+++ crud-spring/src/main/java/com/thales/crudspring/controller/HelloControler.java
@@ -0,0 +1,17 @@
+package com.thales.crudspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloControler {
+
+  @GetMapping
+  public @ResponseBody String hello() {
+    return "Hello, World";
+
+  }
+}
