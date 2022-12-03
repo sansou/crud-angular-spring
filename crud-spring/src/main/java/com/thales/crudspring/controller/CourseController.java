a7e670a4329a76cb2f2902af6b4406130027e3d9
Thales Souza <santos.thales89@gmail.com>
2022-12-03 01:43:04 -0300

    [#15] - Listar Cursos (API HTTP GET)

---
+++ crud-spring/src/main/java/com/thales/crudspring/controller/CourseController.java
@@ -0,0 +1,27 @@
+package com.thales.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thales.crudspring.model.Course;
+import com.thales.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+  private final CourseRepository courseRepository;
+
+  @GetMapping
+  public List<Course> list() {
+    return courseRepository.findAll();
+  }
+
+}
