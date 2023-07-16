1a36edb0648aaf777853a61ecbeb43f23ee5591f
Thales Souza <santos.thales89@gmail.com>
2023-07-16 03:16:48 -0300

    [#26]  - API Spring: Buscar por ID

--- crud-spring/src/main/java/com/thales/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/thales/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,13 @@ public class CourseController {
   @GetMapping
   public List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Course> findById(@PathVariable Long id) {
+    return courseRepository.findById(id)
+        .map(course -> ResponseEntity.ok().body(course))
+        .orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping
