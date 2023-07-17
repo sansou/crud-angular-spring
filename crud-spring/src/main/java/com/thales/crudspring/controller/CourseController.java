03be7a8f49955c9881c6d782a6499be3ebdc8100
Thales Souza <santos.thales89@gmail.com>
2023-07-16 23:56:13 -0300

    [#30] - Atualizar Curso: API Spring HTTP PUT

--- crud-spring/src/main/java/com/thales/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/thales/crudspring/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,18 @@ public class CourseController {
   @PostMapping
   public ResponseEntity<Course> create(@RequestBody Course course) {
     return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    return courseRepository.findById(id)
+        .map(c1 -> {
+          c1.setName(course.getName());
+          c1.setCategory(course.getCategory());
+          var updated = courseRepository.save(c1);
+          return ResponseEntity.ok().body(updated);
+        })
+        .orElse(ResponseEntity.notFound().build());
   }
 
 }
