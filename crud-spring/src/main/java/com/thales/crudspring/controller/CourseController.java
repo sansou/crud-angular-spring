d394a086395370c623968b09aab78a67c7d4b66a
Thales Souza <santos.thales89@gmail.com>
2024-02-18 03:19:28 -0300

    [#35] - Validações da API com Java Bean Validation

--- crud-spring/src/main/java/com/thales/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/thales/crudspring/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.thales.crudspring.model.Course;
 import com.thales.crudspring.repository.CourseRepository;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 
+@Validated // necessario para validar as validções dentro dos métodos
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
@@ -31,19 +36,19 @@ public class CourseController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Course> findById(@PathVariable Long id) {
+  public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id) {
     return courseRepository.findById(id)
         .map(course -> ResponseEntity.ok().body(course))
         .orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping
-  public ResponseEntity<Course> create(@RequestBody Course course) {
+  public ResponseEntity<Course> create(@RequestBody @Valid Course course) {
     return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+  public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course) {
     return courseRepository.findById(id)
         .map(c1 -> {
           c1.setName(course.getName());
@@ -55,7 +60,7 @@ public class CourseController {
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> delete(@PathVariable Long id) {
+  public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id) {
     return courseRepository.findById(id).map(courseFound -> {
       courseRepository.deleteById(id);
       return ResponseEntity.noContent().<Void>build();
--- crud-spring/src/main/java/com/thales/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/thales/crudspring/model/Course.java
@@ -1,10 +1,15 @@
 package com.thales.crudspring.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -15,9 +20,15 @@ public class Course {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  @NotBlank
+  @NotNull
+  @Length(min = 5, max = 200)
   @Column(length = 200, nullable = false)
   private String name;
 
+  @NotNull
+  @Length(max = 20)
+  @Pattern(regexp = "back-end|front-end")
   @Column(length = 20, nullable = false)
   private String category;
 
